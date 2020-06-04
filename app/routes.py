a016a38502dc294175d4d822b114819ef9a18055
Will Xiong <will_xiong@hotmail.com>
2020-06-04 16:18:42 -0700

    Added Images, fixing bugs for sizing

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
 from app import app
 from flask import url_for, render_template
 
-
-
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/about_me')
+def about_me():
+    return render_template('about_me.html')
